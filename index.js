1b68868779e20001a7b5ef49b43d09a29bfaac21
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-13 10:09:58 -0400

    commit tree 683e8662428357f371b9b9d63f63a3f8f4d41edd

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy','Claudia'];
+//Return First Two Drivers
+function returnFirstTwoDrivers(){
+ return drivers.slice(0,2)
+}
+
+//Return Last Two Drivers
+function returnLastTwoDrivers(){
+    return drivers.slice(2,4)
+}
+
+//Selecting Drivers
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+//Create Fare Multiplier
+function createFareMultiplier(cost){
+    return function (fareMultiplier){
+        return cost * fareMultiplier
+    }
+}
+
+//Fare Doubler
+const fareDoubler = createFareMultiplier(2)
+
+//Fare Triplers
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, callback){
+    return callback();
+    console.log(callback)
+}
